b5c81f7ed5b3cadc8bc144b6ece39ebb489b21e6
Preensikl <Preensikl@mail.ru>
2023-05-06 10:52:48 +0300

    Version 9.4

--- gracefull_creator.py
+++ gracefull_creator.py
@@ -12,19 +12,22 @@ class ConfigFiles:
     
     @classmethod
     def gracefull_create(self):
-        self.obj = {}
-        self.obj["type"] = os.getenv('type')
-        self.obj["project_id"] = os.getenv("project_id")
-        self.obj["private_key_id"] = os.getenv("private_key_id")
-        self.obj["private_key"] = os.getenv("private_key")
-        self.obj["client_email"] = os.getenv("client_email")
-        self.obj["client_id"] = os.getenv("client_id")
-        self.obj["auth_uri"] = os.getenv("auth_uri")
-        self.obj["token_uri"] = os.getenv("token_uri")
-        self.obj["auth_provider_x509_cert_url"] = os.getenv("auth_provider_x509_cert_url")
-        self.obj["client_x509_cert_url"] = os.getenv("client_x509_cert_url")
-        with open("gracefull_obj.json", "w") as f:
-            json.dump(self.obj, f)
+        try:
+            self.obj = {}
+            self.obj["type"] = os.getenv('type')
+            self.obj["project_id"] = os.getenv("project_id")
+            self.obj["private_key_id"] = os.getenv("private_key_id")
+            self.obj["private_key"] = os.getenv("private_key")
+            self.obj["client_email"] = os.getenv("client_email")
+            self.obj["client_id"] = os.getenv("client_id")
+            self.obj["auth_uri"] = os.getenv("auth_uri")
+            self.obj["token_uri"] = os.getenv("token_uri")
+            self.obj["auth_provider_x509_cert_url"] = os.getenv("auth_provider_x509_cert_url")
+            self.obj["client_x509_cert_url"] = os.getenv("client_x509_cert_url")
+            with open("gracefull_obj.json", "w") as f:
+                json.dump(self.obj, f)
+        except Exception as e:
+            Decoration._write_logs(e)
             
     @classmethod
     def constants_create(self):
